9c771288a45c027e01b996e7cdc16e85b348a1d0
GianfrancoRocco <gianfrancorocco.business@gmail.com>
2020-08-23 14:32:44 -0300

    Favicon

--- index.php
+++ index.php
@@ -92,6 +92,7 @@ if($_POST){
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/ico" href="favicon/favicon.ico">
  <title>ABM Clientes</title>
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
